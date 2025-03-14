60183162fc1711a6b2537c7f0e654a231b1dd60f
Cristian D. Moreno - Kyonax <kyonax25@gmail.com>
2025-03-14 08:17:40 -0500

    Fixing Pipeline Errors
    Modified-by: Cristian D. Moreno - Kyonax <kyonax25@gmail.com>

--- src/components/AgendaItem.js
+++ src/components/AgendaItem.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function AgendaItem({ item, checkState, toggleCheck, path = [] }) {
   const currentPath = [...path, item.title];
